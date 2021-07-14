a5f2abfdc212d018dc3970b203f8d47357728f72
Thomas Smith <thsm@umich.edu>
2021-07-13 23:56:46 -0400

    Add documentation to monad.ts

--- src/scripts/monad.ts
+++ src/scripts/monad.ts
@@ -1,7 +1,12 @@
 type Then<T, U> = (value: T) => Monad<U>;
 
 interface Monad<T> {
+  // Applies `then` to the value(s) "inside" the monad. This should return the
+  //  same kind of monad as it is called on, possibly with a different type
+  //  parameter.
   bind<U>(then: Then<T, U>): Monad<U>;
+
+  // Creates a monad of this type containing `value`.
   pure(value: T): Monad<T>;
 }
 
