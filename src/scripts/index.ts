3189c0ddbe04a228d5323685dc93849e94b95c1c
Thomas Smith <thsm@umich.edu>
2021-07-23 23:57:57 -0400

    Add documentation to index.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,6 +4,7 @@ import display from "./display";
 import * as dom from "./dom";
 import {fatalError} from "./standard";
 
+// This is needed so `cli` can be added to `window`.
 declare global {
   interface Window {
     cli: typeof cli,
@@ -11,6 +12,8 @@ declare global {
 }
 
 window.cli = cli;
+
+// Displays the controls for the main table and the main table itself.
 displayControls(
   dom.select(".controls") ?? fatalError("Could not find .controls"));
 display(dom.select(".data") ?? fatalError("Could not find .data"));
