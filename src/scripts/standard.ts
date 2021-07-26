25187883ad17c9f070bae45b6a717cd2a2d6015e
Thomas Smith <thsm@umich.edu>
2021-07-25 23:50:46 -0400

    Add documentation to standard.ts

--- src/scripts/standard.ts
+++ src/scripts/standard.ts
@@ -1,9 +1,12 @@
+// Assert that `value` is true. If not, throw an Error (optionally with a
+//  specified message).
 function assert(value: boolean, message?: string): asserts value {
   if (!value) {
     throw Error(message ?? "Assertion failed");
   }
 }
 
+// Throw an Error (optionally with a specified message).
 function fatalError(message?: string): never {
   throw Error(message ?? "Fatal error");
 }
